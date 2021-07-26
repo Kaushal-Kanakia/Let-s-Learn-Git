26b17c14799f13424187dac00e710e2bdc8ea5a1
Kaushal Kanakia <kaushalkanakia33@gmail.com>
2021-07-26 14:40:47 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+public class Sum {
+
+    public static void main(String args[])
+    {
+        int a = 2;
+        int b = 6;
+
+        System.out.println("The multiplication is "+(a*b));
+    }
+
+}
